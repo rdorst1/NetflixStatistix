f3daa17bbca9c4fe3f52816d9d2172d1cc5610fd
Rowan Dorst <rdorst1@avans.nl>
2018-01-13 22:23:58 +0100

    Project commit

---
+++ src/netflixStatistix/Main.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package netflixStatistix;
+
+/**
+ *
+ * @author rowan
+ */
+import java.sql.*;
+
+/**
+ * Dit is een voorbeeld Java toepassing waarin je verbinding maakt met een SQLServer database.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Geeft het uiterlijk van de applicatie.
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(UserInterface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(UserInterface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(UserInterface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(UserInterface.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //Start de UserInterface
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new netflixStatistix.UserInterface().setVisible(true);
+            }
+        });
+
+        // Dit zijn de instellingen voor de verbinding. Vervang de databaseName indien deze voor jou anders is.
+        String connectionUrl = "jdbc:sqlserver://localhost\\SQLEXPRESS1;databaseName=Bibliotheek;integratedSecurity=true;";
+
+        // Connection beheert informatie over de connectie met de database.
+        Connection con = null;
+
+        // Statement zorgt dat we een SQL query kunnen uitvoeren.
+        Statement stmt = null;
+
+        // ResultSet is de tabel die we van de database terugkrijgen.
+        // We kunnen door de rows heen stappen en iedere kolom lezen.
+        ResultSet rs = null;
+
+        try {
+            // 'Importeer' de driver die je gedownload hebt.
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            // Maak de verbinding met de database.
+            con = DriverManager.getConnection(connectionUrl);
+
+            // Stel een SQL query samen.
+            String SQL = "SELECT TOP 10 * FROM Boek";
+            stmt = con.createStatement();
+            // Voer de query uit op de database.
+            rs = stmt.executeQuery(SQL);
+
+            System.out.print(String.format("| %7s | %-32s | %-24s |\n", " ", " ", " ").replace(" ", "-"));
+
+            // Als de resultset waarden bevat dan lopen we hier door deze waarden en printen ze.
+            while (rs.next()) {
+                // Vraag per row de kolommen in die row op.
+                int ISBN = rs.getInt("ISBN");
+                String title = rs.getString("Titel");
+                String author = rs.getString("Auteur");
+
+                // Print de kolomwaarden.
+                // System.out.println(ISBN + " " + title + " " + author);
+
+                // Met 'format' kun je de string die je print het juiste formaat geven, als je dat wilt.
+                // %d = decimal, %s = string, %-32s = string, links uitgelijnd, 32 characters breed.
+                System.out.format("| %7d | %-32s | %-24s | \n", ISBN, title, author);
+            }
+            System.out.println(String.format("| %7s | %-32s | %-24s |\n", " ", " ", " ").replace(" ", "-"));
+
+        }
+
+        // Handle any errors that may have occurred.
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        finally {
+            if (rs != null) try { rs.close(); } catch(Exception e) {}
+            if (stmt != null) try { stmt.close(); } catch(Exception e) {}
+            if (con != null) try { con.close(); } catch(Exception e) {}
+        }
+    }
+}
